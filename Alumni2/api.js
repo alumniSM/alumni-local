0a2caf05203ea2e2e09dd98c3fd530d354d15b8d
alumniSM <simplifyweb03@gmail.com>
2024-11-05 03:35:37 +0300

    Update api.js

--- Alumni2/api.js
+++ Alumni2/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://alumni-local.onrender.com", // adjust this to your backend URL
+  baseURL: "https://alumni-local.onrender.com/api", // adjust this to your backend URL
 });
 
 api.interceptors.request.use(
